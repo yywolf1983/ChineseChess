238bd69516f614f89821d42e5ec433efe216b3e7
myphone <yywolf1983@gmail.com>
2026-07-07 12:35:11 +0800

    引入注册机

---
+++ app/src/main/java/top/nones/chessgame/ChessApplication.java
@@ -0,0 +1,15 @@
+package top.nones.chessgame;
+
+import android.app.Application;
+import com.reggate.lib.RegGateConfig;
+
+public class ChessApplication extends Application {
+    @Override
+    public void onCreate() {
+        RegGateConfig.init(this)
+                .mainActivity(PvMActivity.class)
+                .trialDays(7)
+                .build();
+        super.onCreate();
+    }
+}
